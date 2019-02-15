8821fe599b66fca20ae172e67b41af26bf30b089
Nick.Selivanov <nick.selivanov@gmail.com>
2019-02-15 18:14:33 +0300

    Пробная синхронизация (изменен index.js (оба задания в одном файле) и создан README.txt)

--- index.js
+++ index.js
@@ -2,8 +2,20 @@
 const express = require ('express');
 const app = express ();
 // Обработка параметров ссылки
-app.get('/settings/:messageId', function(request, response){
+app.get('/index', function (req, res, next) {
+    res.send("Главная страница /index");
+    next;
+});
+app.get('/about', function (req, res, next) {
+    res.send("Страница описания /about");
+    next;
+});
+app.get('/settings', function (req, res, next) {
+    res.send("Страница настроек /settings");
+    next;
+});
+app.get('/settings/:messageId', function(req,res){
     // Вывод сообщения на страницу
-    response.send('Привет ' + request.params['messageId'])
+    res.send('<h1>Страница настроек /settings</h1>' + 'Привет ' + req.params['messageId'])
 });
 app.listen(8080)
