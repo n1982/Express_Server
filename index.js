6e2adae299ad1739234d849b6028d701cc3a5e5f
Nick.Selivanov <nick.selivanov@gmail.com>
2019-02-17 16:38:27 +0300

    Исправления в Index.js (как в app.js)

--- index.js
+++ index.js
@@ -4,48 +4,43 @@ const app = express ();
 //Создание константы
 const auth = false;
 // Проверка значения auth
-
-app.use('/index', function(req, res, next) {
-    if (auth == false){
-        res.redirect('http://localhost:8080/login')
-    } else app.get('/index', function (req, res, next) {
+app.use((req, res, next)=>{
+    if (!auth && req.url != '/login'){
+        res.redirect('/login');
+    } else{
+        next();
+    }
+});
+app.get('/index', function (req, res) {
         res.send("Главная страница /index");
-        next;
-    })
-})
-app.use('/about', function(req, res, next) {
-    if (auth == false){
-        res.redirect('http://localhost:8080/login')
-    } else app.get('/about', function (req, res, next) {
+});
+
+app.get('/about', function (req, res) {
         res.send("Страница описания /about");
-        next;
-    })
-})
-app.use('/settings', function(req, res, next) {
-    if (auth == false){
-        res.redirect('http://localhost:8080/login')
-    } else app.get('/settings', function (req, res, next) {
+});
+app.get('/settings', function (req, res, next) {
         res.send("Страница настроек /settings");
-        next;
-    })
-})
-// Обработка параметров ссылки
+});
 
 // Сюда не доходит,переадресовывается по /settings
 app.get('/settings/:messageId', function(req, res, next){
     // Вывод сообщения на страницу
-    res.send('<h1>Страница настроек /settings</h1>' + 'Привет ' + req.params['messageId'])
+    res.send('<h1>Страница настроек /settings</h1>' + 'Привет ' + req.params['messageId']);
     next;
 });
 // Пересылает сюда после app.use (/index, /settings, /about)
 app.get('/login', function (req, res, next) {
     res.send('Страница входа /login');
     next;
-})
+});
 //если адресов нет в списке
+app.get('/404', function(req, res){
+    res.sendStatus(404)
+});
+app.get('*', function (req, res) {
+    res.redirect('/404');
+});
 
-app.get('/*', function (req, res, next) {
-    res.send('Страница 404');
-})
-
-app.listen(8080)
+app.listen(8080, ()=> {
+    console.log('Слушается порт 8080');
+});
